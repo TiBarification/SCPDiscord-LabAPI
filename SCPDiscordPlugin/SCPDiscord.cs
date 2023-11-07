313da5ff1c96d43222e2b4b16f63e913f6f7af01
Karl Essinger <xkaess22@gmail.com>
2023-11-07 01:45:16 +0100

    Version bump

--- SCPDiscordPlugin/SCPDiscord.cs
+++ SCPDiscordPlugin/SCPDiscord.cs
@@ -30,7 +30,7 @@ namespace SCPDiscord
 
 		public bool shutdown;
 
-		public const string VERSION = "3.1.4";
+		public const string VERSION = "3.1.5";
 
 		[PluginEntryPoint("SCPDiscord", VERSION, "SCP:SL - Discord bridge.", "Karl Essinger")]
 		public void Start()
