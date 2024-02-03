0cd38c0402d93726e9357b2f96f8d5de2c3c87bb
Karl Essinger <xkaess22@gmail.com>
2024-02-03 19:58:56 +0100

    Added some info about players to language file

--- SCPDiscordPlugin/APIExtensions.cs
+++ SCPDiscordPlugin/APIExtensions.cs
@@ -40,33 +40,33 @@ namespace SCPDiscord
 		{
 			if (variables == null) return;
 
-			variables.AddIfNotExist(prefix + "-ipaddress",        player?.IpAddress);
-			variables.AddIfNotExist(prefix + "-name",             player?.Nickname);
-			variables.AddIfNotExist(prefix + "-id",               player?.PlayerId.ToString());
-			variables.AddIfNotExist(prefix + "-userid",           player?.GetParsedUserID());
-			variables.AddIfNotExist(prefix + "-role",             player?.Role.ToString());
-			variables.AddIfNotExist(prefix + "-team",             player?.ReferenceHub.GetTeam().ToString());
-			variables.AddIfNotExist(prefix + "-donottrack",       player?.DoNotTrack.ToString());
-			variables.AddIfNotExist(prefix + "-health",           player?.Health.ToString(CultureInfo.InvariantCulture));
-			variables.AddIfNotExist(prefix + "-isdisarmed",       player?.IsDisarmed.ToString());
-			variables.AddIfNotExist(prefix + "-isalive",          player?.IsAlive.ToString());
-			variables.AddIfNotExist(prefix + "-ismuted",          player?.IsMuted.ToString());
-			variables.AddIfNotExist(prefix + "-hasreservedslot",  player?.HasReservedSlot.ToString());
-			variables.AddIfNotExist(prefix + "-isglobalmod",      player?.IsGlobalModerator.ToString());
-			variables.AddIfNotExist(prefix + "-isintercommuted",  player?.IsIntercomMuted.ToString());
-			variables.AddIfNotExist(prefix + "-hasfullinventory", player?.IsInventoryFull .ToString());
-			variables.AddIfNotExist(prefix + "-hasnoitems",       player?.IsWithoutItems .ToString());
-			variables.AddIfNotExist(prefix + "-isnortwoodstaff",  player?.IsNorthwoodStaff.ToString());
-			variables.AddIfNotExist(prefix + "-overwatchon",      player?.IsOverwatchEnabled.ToString());
-			variables.AddIfNotExist(prefix + "-noclipon",         player?.IsNoclipEnabled.ToString());
-			variables.AddIfNotExist(prefix + "-godmodeon",        player?.IsGodModeEnabled.ToString());
-			variables.AddIfNotExist(prefix + "-isoutofammo",      player?.IsOutOfAmmo.ToString());
+			variables.AddIfNotExist(prefix + "-ipaddress",            player?.IpAddress);
+			variables.AddIfNotExist(prefix + "-name",                 player?.Nickname);
+			variables.AddIfNotExist(prefix + "-id",                   player?.PlayerId.ToString());
+			variables.AddIfNotExist(prefix + "-userid",               player?.GetParsedUserID());
+			variables.AddIfNotExist(prefix + "-role",                 player?.Role.ToString());
+			variables.AddIfNotExist(prefix + "-team",                 player?.ReferenceHub.GetTeam().ToString());
+			variables.AddIfNotExist(prefix + "-donottrack",           player?.DoNotTrack.ToString());
+			variables.AddIfNotExist(prefix + "-health",               player?.Health.ToString(CultureInfo.InvariantCulture));
+			variables.AddIfNotExist(prefix + "-isdisarmed",           player?.IsDisarmed.ToString());
+			variables.AddIfNotExist(prefix + "-isalive",              player?.IsAlive.ToString());
+			variables.AddIfNotExist(prefix + "-ismuted",              player?.IsMuted.ToString());
+			variables.AddIfNotExist(prefix + "-hasreservedslot",      player?.HasReservedSlot.ToString());
+			variables.AddIfNotExist(prefix + "-isglobalmod",          player?.IsGlobalModerator.ToString());
+			variables.AddIfNotExist(prefix + "-isintercommuted",      player?.IsIntercomMuted.ToString());
+			variables.AddIfNotExist(prefix + "-hasfullinventory",     player?.IsInventoryFull .ToString());
+			variables.AddIfNotExist(prefix + "-hasnoitems",           player?.IsWithoutItems .ToString());
+			variables.AddIfNotExist(prefix + "-isnortwoodstaff",      player?.IsNorthwoodStaff.ToString());
+			variables.AddIfNotExist(prefix + "-overwatchon",          player?.IsOverwatchEnabled.ToString());
+			variables.AddIfNotExist(prefix + "-noclipon",             player?.IsNoclipEnabled.ToString());
+			variables.AddIfNotExist(prefix + "-godmodeon",            player?.IsGodModeEnabled.ToString());
+			variables.AddIfNotExist(prefix + "-isoutofammo",          player?.IsOutOfAmmo.ToString());
 			variables.AddIfNotExist(prefix + "-hasremoteadminaccess", player?.RemoteAdminAccess.ToString());
-			variables.AddIfNotExist(prefix + "-bypassenabled",    player?.IsBypassEnabled.ToString());
-			variables.AddIfNotExist(prefix + "-isscp",            player?.IsSCP.ToString());
-			variables.AddIfNotExist(prefix + "-ismtf",            player?.IsNTF.ToString());
-			variables.AddIfNotExist(prefix + "-ischaos",          player?.IsChaos.ToString());
-			variables.AddIfNotExist(prefix + "-ishuman",          player?.IsHuman.ToString());
+			variables.AddIfNotExist(prefix + "-bypassenabled",        player?.IsBypassEnabled.ToString());
+			variables.AddIfNotExist(prefix + "-isscp",                player?.IsSCP.ToString());
+			variables.AddIfNotExist(prefix + "-ismtf",                player?.IsNTF.ToString());
+			variables.AddIfNotExist(prefix + "-ischaos",              player?.IsChaos.ToString());
+			variables.AddIfNotExist(prefix + "-ishuman",              player?.IsHuman.ToString());
 
 			if (includeDisarmer)
 			{
