cd0a92cf7830fec267ccbf818641513cb4904742
TouDead <72409749+TouDead@users.noreply.github.com>
2024-05-14 22:04:57 +0300

    update using
    I forgot to update using again

--- SCPDiscordPlugin/Utilities.cs
+++ SCPDiscordPlugin/Utilities.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
 using System.Xml;
 using PluginAPI.Core;
 
