291a9260abfaaa5426af3fcb9d6d67a32596df80
Karl Essinger <xkaess22@gmail.com>
2024-07-24 20:42:42 +0200

    Clear log cache if log is turned off

--- SCPDiscordPlugin/Logger.cs
+++ SCPDiscordPlugin/Logger.cs
@@ -77,6 +77,7 @@ namespace SCPDiscord
             {
                 if (string.IsNullOrWhiteSpace(path))
                 {
+                    startupCache.Clear();
                     logFileWriter?.Close();
                     logFileWriter = null;
                     return;
